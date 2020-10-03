b1e9aea53f88cc7caeb707e8a5d169a9b9c1ac6e
Andrew Jones <aej11@hotmail.com>
2020-10-03 13:02:31 -0400

    Added mocking to unit tests

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![build status of master](https://travis-ci.com/aej11a/Github567.svg?branch=master)](https://travis-ci.org/aej11a/Github567)
+[![build status of master](https://travis-ci.com/aej11a/Github567.svg?branch=HW05a_Mocking)](https://travis-ci.org/aej11a/Github567)
--- TestGithubApp.py
+++ TestGithubApp.py
@@ -1,19 +1,37 @@
 import sys
 import unittest
+from unittest.mock import Mock, patch
+
 from GithubApp import getReposList, getCommitsList, listReposWithCounts
 
+
+@patch('GithubApp.requests.get')
 class TestGithub(unittest.TestCase):
-    def testGetReposList(self):
-        self.assertIsNotNone(getReposList("adwqdwqdwqwdsdthtjyj")['message'])
-        repos = getReposList("aej11a")
+
+    sampleRepos = [{'name': 'myrepo', 'id': '123'}]
+    sampleCommits = [{'sha': 'sample'}]
+
+    def testGetReposList(self, mock_get):
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.json.return_value = TestGithub.sampleRepos
+
+        repos = getReposList("testUser")
         self.assertTrue(len(repos) > 0)
         self.assertIsNotNone(repos[0]['id'])
 
-    def testGetCommitsList(self):
-        self.assertIsNotNone(getCommitsList("adwqdwqdwqwdsdthtjyj", "test")['message'])
-        commits = getCommitsList("aej11a", "excalidraw")
+    def testGetCommitsList(self, mock_get):
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.json.return_value = TestGithub.sampleCommits
+
+        commits = getCommitsList("aej11a", "myrepo")
         self.assertTrue(len(commits) > 0)
         self.assertIsNotNone(commits[0]['sha'])
 
-    def testListReposWithCounts(self):
-        self.assertTrue("Repo: excalidraw Number of commits: 30" in listReposWithCounts("aej11a"))
+    def testListReposWithCounts(self, mock_get):
+        # Mock two responses
+        multiple_responses = [Mock(ok=True), Mock(ok=True)]
+        multiple_responses[0].json.return_value = TestGithub.sampleRepos
+        multiple_responses[1].json.return_value = TestGithub.sampleCommits
+        mock_get.side_effect = multiple_responses
+
+        self.assertTrue("Repo: myrepo Number of commits: 1" in listReposWithCounts("aej11a"))
